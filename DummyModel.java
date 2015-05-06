3329b6c96c6476f58094f86bfa9e9a6ac1bdf8c9
Magnus Hagmar <hagmar.magnus@gmail.com>
2015-05-06 18:24:24 +0200

    fix

--- DummyModel.java
+++ DummyModel.java
@@ -17,31 +17,8 @@ public class DummyModel implements IBouncingBallsModel {
 		this.areaHeight = height;
 		
 		balls = new LinkedList<Ball>();
-		balls.add(new Ball("two", 1, 5, 1, 24, 5, 0.1, 9.81));
-		balls.add(new Ball("two", 6, 5, 2, 23, 5, 0.1, 9.81));
-		balls.add(new Ball("one", 1, 1, 3, 22, 5, 0.1, 9.81));
-		balls.add(new Ball("one", 1, 1, 4, 21, 1, 0.1, 9.81));
-		balls.add(new Ball("one", 1, 1, 5, 20, 14, 0.1, 9.81));
-		balls.add(new Ball("one", 1, 1, 6, 19, 5, 0.1, 9.81));
-		balls.add(new Ball("one", 1, 1, 7, 18, 5, 0.1, 9.81));
-		balls.add(new Ball("one", 1, 1, 8, 17, 9, 0.1, 9.81));
-		balls.add(new Ball("two", 1, 5, 9, 16, 5, 0.1, 9.81));
-		balls.add(new Ball("two", 6, 5, 10, 15, 5, 0.1, 9.81));
-		balls.add(new Ball("one", 1, 1, 11, 14, 5, 0.1, 9.81));
-		balls.add(new Ball("one", 1, 1, 12, 13, 1, 0.1, 9.81));
-		balls.add(new Ball("one", 1, 1, 13, 12, 14, 0.1, 9.81));
-		balls.add(new Ball("one", 1, 1, 14, 11, 5, 0.1, 9.81));
-		balls.add(new Ball("one", 1, 1, 15, 10, 5, 0.1, 9.81));
-		balls.add(new Ball("one", 1, 1, 16, 9, 9, 0.1, 9.81));
-		balls.add(new Ball("two", 1, 5, 17, 8.3, 5, 0.1, 9.81));
-		balls.add(new Ball("two", 6, 5, 18, 7.3, 5, 0.1, 9.81));
-		balls.add(new Ball("one", 1, 1, 19, 6, 5, 0.1, 9.81));
-		balls.add(new Ball("one", 1, 1, 20, 5, 1, 0.1, 9.81));
-		balls.add(new Ball("one", 1, 1, 21, 4, 14, 0.1, 9.81));
-		balls.add(new Ball("one", 1, 1, 22, 3, 5, 0.1, 9.81));
-		balls.add(new Ball("one", 1, 1, 23, 2, 5, 0.1, 9.81));
-		balls.add(new Ball("one", 1, 1, 24, 1, 9, 0.1, 9.81));
-
+		balls.add(new Ball("two", 1, 1, 1, 4, 5, 1, 9.81));
+		balls.add(new Ball("two", 1, 1, 1, 4, 10, 1, 9.81));
 	}
 
 	private void calculateBounce(Ball a, Ball b){
